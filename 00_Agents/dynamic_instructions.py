bcfbf7bccce3f70d716b7418926e567294b3b9ef
Amraha-Anwar <amrahaanwar@gmail.com>
2025-08-07 02:23:09 +0500

    .

---
+++ 00_Agents/dynamic_instructions.py
@@ -0,0 +1,45 @@
+from agents import Agent, OpenAIChatCompletionsModel, AsyncOpenAI, Runner, set_tracing_disabled, RunContextWrapper
+from dotenv import load_dotenv
+from dataclasses import dataclass
+import os
+
+load_dotenv()
+set_tracing_disabled(True)
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+url = os.getenv("BASE_URL")
+
+external_client = AsyncOpenAI(
+    api_key = gemini_api_key,
+    base_url = url
+)
+
+model = OpenAIChatCompletionsModel(
+    model= 'gemini-2.5-flash',
+    openai_client = external_client
+)
+
+@dataclass
+class UserInfo:
+    name : str
+    age : int
+
+def dynamic_instruction(context: RunContextWrapper[UserInfo], agent : Agent[UserInfo]) -> str:
+    return f"The user's name is {context.context.name} who is {context.context.age} years old."
+
+
+user_info = UserInfo(name = "Amraha", age = 18)
+
+agent = Agent[UserInfo](
+    name = "Assistant",
+    instructions = dynamic_instruction,
+    model = model
+)
+
+result = Runner.run_syn(
+    agent,
+    "What do you know about me?",
+    context = user_info
+)
+
+print(result.final_output)
